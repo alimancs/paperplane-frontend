bf3b99fea2575dc6a705969a05dda503825ea07e
alimancs <aliman2952003@gmail.com>
2024-10-10 16:13:12 +0100

    changing post data sending to backend format

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -25,13 +25,13 @@ export default function CreatePost() {
         e.preventDefault();
         const authToken = localStorage.getItem('authToken');
         const dataStr = `title===${title};;;summary===${summary};;;content===${content};;;cover===${file}`;
-        const data = new FormData();
-        data.set( 'title', title );
-        data.set( 'summary', summary );
-        data.set( 'content', content );
-        data.set( 'cover', file );
+        // const data = new FormData();
+        // data.set( 'title', title );
+        // data.set( 'summary', summary );
+        // data.set( 'content', content );
+        // data.set( 'cover', file );
         
-        console.log(data);
+        // console.log(data);
         
         const response = await  fetch("https://paperplane-blog-api.onrender.com/addpost", {
             method:"POST",
