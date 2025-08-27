e9a62dbb02bff352dcffbc2778b794575bfc1d48
alimancs <aliman2952003@gmail.com>
2025-08-27 05:14:35 +0100

    update to profile page

--- src/components/ui/EllipsisOption.tsx
+++ src/components/ui/EllipsisOption.tsx
@@ -3,13 +3,40 @@ import React from "react";
 
 interface EllipsisOptionProps {
     list: { label: string; onClick: () => void }[];
+    className?: string;
 }
 
-const EllipsisOption:React.FC<EllipsisOptionProps> = ( { list } ) => {
+const EllipsisOption:React.FC<EllipsisOptionProps> = ( { list, className='' } ) => {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const handleOptionClick = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
-        <button className="cursor-pointer">
-            <Ellipsis className="h-5 w-5 text-gray-500 hover:text-gray-900"/>
-        </button>
+        <div className={`w-fit ${className}`}>
+            <button className="cursor-pointer" onClick={handleOptionClick}>
+                <Ellipsis className="w-5 text-gray-500 hover:text-gray-900"/>
+            </button>
+            {isOpen && (
+                <div className="absolute w-fit bg-white border border-gray-200 flex flex-col justify-center items-center rounded rounded-tl-0 shadow-lg z-10">
+                    
+                    <ul className="py-1 w-full">
+                        {list.map((item, index) => (
+                            <li className="w-full" key={index}>
+                                <button
+                                    onClick={item.onClick}
+                                    className="block px-6 cursor-pointer w-full py-2 text-sm text-gray-400 hover:text-gray-700"
+                                >
+                                    {item.label}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
     );
 };
 
--- src/pages/profile/ProfileHome.tsx
+++ src/pages/profile/ProfileHome.tsx
@@ -1,4 +1,4 @@
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, Lock, UserCircle2 } from "lucide-react";
 import React, { use } from "react";
 import colors from "../../utils/colors";
 import { useNavigate } from "react-router-dom";
@@ -12,9 +12,10 @@ const ProfileHome: React.FC = () => {
             <div className="w-full flex flex-row justify-between items-center">
                 <span className={`text-[40px] font-bold text-[${colors.primary}]`}>Elon Musk</span>
                 <EllipsisOption
+                    className="mt-3"
                     list={[
-                        { label: "Edit Profile", onClick: () => navigate('/@profile/edit') },
-                        { label: "Settings", onClick: () => navigate('/@profile/settings') },
+                        { label: "Copy link to profile", onClick: () => navigate('/@profile/edit') },
+                        { label: "Design to profile", onClick: () => navigate('/@profile/settings') },
                     ]}
                 />
             </div>
@@ -26,6 +27,33 @@ const ProfileHome: React.FC = () => {
                 <button onClick={() => navigate('/@profile/about')} className="text-[14px] pb-4 border-b-[0.1px] text-gray-400 cursor-pointer hover:text-gray-900 border-b-transparent">
                     About
                 </button>
+            </div>
+            <div className="w-full h-[130px] bg-gray-100 flex flex-row justify-between border-gray-200 border-[0.1px] rounded-[2px]">
+                <div className="w-[59%] h-full px-[4%] flex flex-col gap-2 justify-center">
+                    <div className="flex flex-row gap-2 items-center">
+                        <UserCircle2 className="h-5 w-5 text-gray-400"/>
+                        <span className="text-black text-[14px]">Elon Musk</span>
+                    </div>
+                    <span className={`text-[18px] font-extrabold text-[${colors.primary}]`}>Reading list</span>
+                    <div className="w-full flex flex-row justify-between items-center">
+                        <div className="flex flex-row items-center">
+                            <span className="text-[14px]">No stories</span>
+                            <Lock className="h-3 w-3 text-gray-700 inline ml-2"/>
+                        </div>
+                        <EllipsisOption
+                            
+                            list={[
+                                { label:"Copy link", onClick:()=>{console.log("Copy link")} },
+                                { label:"Edit list info", onClick:()=>{console.log("Copy link")} },
+                                { label:"Make list public", onClick:()=>{console.log("Copy link")} },
+                                { label:"Hide responses", onClick:()=>{console.log("Copy link")} },
+                            ]}
+                        />
+                    </div>
+                </div>
+                <div className="w-[19%] h-full bg-gray-200"></div>
+                <div className="w-[13%] h-full bg-gray-200"></div>
+                <div className="w-[8%] h-full bg-gray-200"></div>
             </div>
         </div>
     );
